d1f0aed3cdc85b93c8e1dc0454b3bc5bf8a6b95c
Trai Lynne <trailynne@gmail.com>
2019-06-07 10:43:40 -0600

    Define stack class

--- graph_adventure/adv.py
+++ graph_adventure/adv.py
@@ -25,6 +25,22 @@ traversalPath = ['n', 's', 'w', 'e']
 
 traversalGraph = {}
 
+# Stack Class
+class Stack:
+    def __init__(self):
+        self.stack = []
+    def push(self, value): 
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+
+stack = Stack()
+
 # TRAVERSAL TEST
 visited_rooms = set()
 player.currentRoom = world.startingRoom
